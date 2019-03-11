bca8c35f0a3d2aae3b66daf756ca7737d7468ddf
ataricoder <ataricoder@gmail.com>
2019-03-10 23:49:44 -0500

    added image support with parse, and added source code info.plist to fetch image over HTTP

---
+++ Danystagram/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Danystagram
+//
+//  Created by ataricoder on 3/10/19.
+//  Copyright © 2019 danielmunoz. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
